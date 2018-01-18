736d8ffddb1601940daea528c1ba3a6b7e8471a4
Oliver Viljamaa <oliver.viljamaa@gmail.com>
2018-01-18 13:57:56 +0000

    Add LESS support and Bootstrap to docs

--- docs/Docs.js
+++ docs/Docs.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './Docs.less';
+
 const Docs = () => <div className="container">Here shall be docs.</div>;
 
 export default Docs;
--- webpack.config.js
+++ webpack.config.js
@@ -6,15 +6,24 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'public-navigation.min.js',
     library: '@transferwise/public-navigation',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ]
-  }
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.less$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'less-loader'],
+      },
+      {
+        test: /\.(woff|woff2|svg|eot|ttf)/,
+        loader: 'file-loader',
+      },
+    ],
+  },
 };
